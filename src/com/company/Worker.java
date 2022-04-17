2e09e05022e212a49a1171001657e10b288d2795
IgorGultyaev <igor.gultyaev@mail.ru>
2022-04-17 20:05:02 +0300

    final

--- src/com/company/Worker.java
+++ src/com/company/Worker.java
@@ -4,15 +4,17 @@ public class Worker {
 
     OnTaskDoneListener callback;
     OnTaskErrorListener errorCallback;
+    int errorNum;
 
     public Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback, int errorNum) {
         this.callback = callback;
         this.errorCallback = errorCallback;
+        this.errorNum = errorNum;
     }
 
     public void start() {
         for (int i = 0; i < 100; i++) {
-            if (i == 33) {
+            if (i == errorNum) {
                 errorCallback.onError("Task " + i + " is error");
             } else {
                 callback.onDone("Task " + i + " is done");
