67d9964ecaaf9f87e6efb291437e631e3f0b7327
IgorGultyaev <igor.gultyaev@mail.ru>
2022-04-17 15:05:33 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        OnTaskDoneListener listener = System.out::println;
+        OnTaskErrorListener taskError=System.out::println;
+
+        Worker worker = new Worker(listener,taskError, 33);
+        worker.start();
+
+    }
+}
